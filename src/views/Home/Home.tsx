0fe3f219fb7a8ec79f6bf3b4fd63877a1bb28285
PiotrWR <pwrynio@gmail.com>
2021-10-12 00:17:51 +0200

    Fix folder name of paginate component

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SpacecraftList from 'components/organisms/Spacecraft/SpacecraftList';
-import Paginator from 'components/molecules/Paginator/Paginate';
+import Paginator from 'components/molecules/Paginate/Paginate';
 import { Wrapper } from './Home.styles';
 
 const Home = () => {
