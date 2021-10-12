2d40ecdce8ba23f155b011dcb1e2e26241052853
PiotrWR <pwrynio@gmail.com>
2021-10-12 21:28:46 +0200

    Fix browser warning about fonts css import

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { theme } from 'assets/styles/theme';
 import { GlobalStyles } from 'assets/styles/GlobalStyles';
 import { AppProvider } from 'context/AppContext';
@@ -12,27 +13,39 @@ import BackgroundWrapper from './BackgroundWrapper';
 
 const queryClient = new QueryClient();
 
+const InitialHelmet = () => (
+  <Helmet>
+    <title>Space Land - HOME</title>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+    </style>
+  </Helmet>
+);
+
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <AppProvider>
-          <Router>
-            <Wrapper>
-              <BackgroundWrapper>
-                <NavWrapper>
-                  <Navbar />
-                </NavWrapper>
-                <MainContentWrapper>
-                  <Home />
-                </MainContentWrapper>
-              </BackgroundWrapper>
-            </Wrapper>
-          </Router>
-        </AppProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <HelmetProvider>
+      <InitialHelmet />
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <AppProvider>
+            <Router>
+              <Wrapper>
+                <BackgroundWrapper>
+                  <NavWrapper>
+                    <Navbar />
+                  </NavWrapper>
+                  <MainContentWrapper>
+                    <Home />
+                  </MainContentWrapper>
+                </BackgroundWrapper>
+              </Wrapper>
+            </Router>
+          </AppProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 }
 
