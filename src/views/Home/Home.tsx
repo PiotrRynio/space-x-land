f71262b943e04b2dfef8d981addcc107416ae605
PiotrWR <pwrynio@gmail.com>
2021-10-13 20:17:32 +0200

    Fix imports

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SpacecraftList from 'components/organisms/Spacecraft/SpacecraftList';
+import SpacecraftList from 'components/organisms/SpacecraftList/SpacecraftList';
 import Paginator from 'components/molecules/Paginate/Paginate';
 import { Wrapper } from './Home.styles';
 
