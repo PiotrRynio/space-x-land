855a7636332314c42526948155647c08b2c326a1
PiotrWR <pwrynio@gmail.com>
2021-10-14 08:58:31 +0200

    Add simply integration tests

--- src/views/Home/Home.test.tsx
+++ src/views/Home/Home.test.tsx
@@ -1,5 +1,4 @@
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
-import { within } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved, fireEvent } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import { handlers } from 'mock/msw/handlers/space-x-land/handlers';
 import Home from './Home';
@@ -50,5 +49,24 @@ describe('Home page:', () => {
 
     const spacecraftItems = screen.queryAllByTestId('spacecraft-list-item');
     expect(spacecraftItems.length).toBe(6);
+  });
+
+  test(`when spacecrafts items are fetched and user click on button "2" on paginate, then second page of items will displayed`, async () => {
+    const { componentWithTestsWrappers } = addTestsWrappers({
+      toBeWrappedComponent: <Home />,
+    });
+    render(componentWithTestsWrappers());
+    await waitForElementToBeRemoved(() => screen.queryByRole(`status`));
+
+    fireEvent.click(screen.getByLabelText(/Page 2/i));
+
+    const article1 = screen.queryByText(/launch was Boeing'/i);
+    const article2 = screen.queryByText(/TürkmenÄlem 52°E/i);
+    const article3 = screen.queryByText(/Launch performance was nominal until an/i);
+    const article4 = screen.queryByText(/NEXT Mission/i);
+    expect(article1).toBeInTheDocument();
+    expect(article2).toBeInTheDocument();
+    expect(article3).toBeInTheDocument();
+    expect(article4).toBeInTheDocument();
   });
 });
